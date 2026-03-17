3ab4d23141f0c54c6bd6e540c8748fcc214ae11f
Peter Jur <pmakec@gmail.com>
2026-03-18 01:12:53 +0200

    Frontend deployment changes

--- client/src/api/http.js
+++ client/src/api/http.js
@@ -1,9 +1,18 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1",
+  // baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1",
+  // baseURL: "https://uniportal-8ejm.onrender.com/api/v1",
+  // headers: {
+  //   "Content-Type": "application/json",
+  // },
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
+// Attach token automatically
 http.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
